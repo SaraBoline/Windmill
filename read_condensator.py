bac164fdb28afbbc8dc1550af2e01d0f706591d9
SaraBoline <142791749+SaraBoline@users.noreply.github.com>
2025-05-06 21:38:41 +0200

    Update read_condensator.py

--- read_condensator.py
+++ read_condensator.py
@@ -1,10 +1,10 @@
 from machine import ADC, Pin
 import time
 
-# Initialize ADC on GPIO 28 (which is ADC2)
+# definere adc på Pin (gpio 28)
 adc = ADC(Pin(28))
 
-# Infinite loop to read every 0.5 seconds
+# læs den her pin i et loop
 while True:
     value = adc.read_u16()  # Reads a 16-bit value (0–65535)
     print("ADC Reading:", value)
@@ -15,10 +15,8 @@ while True:
 from machine import ADC, Pin
 import time
 
-# Initialize ADC on GPIO 28 (which is ADC2)
 adc = ADC(Pin(28))
 
-# Infinite loop to read every 0.5 seconds
 while True:
     value = adc.read_u16()  # Reads a 16-bit value (0–65535)
     voltage = (value / 65535) * 3.3  # Convert to voltage
